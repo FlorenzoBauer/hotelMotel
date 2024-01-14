5647bfbc480d454d0008e267b4d3ae51e0238ee1
Florenzo Bauer <florenzobauer@gmail.com>
2024-01-14 15:18:19 -0700

    Iteration 1 complete, need to work on actual looks, but should be complete. Need to add sections to separate past and present bookings.

--- src/bookings.js
+++ src/bookings.js
@@ -1,7 +1,14 @@
 import { bookins } from '../data/bookings-data';
 
-export const findBookings = (customer) => {
-
-    customer.bookings =  bookins.filter(booking => booking.userID === customer.id);
+export const findBookings = (customer, allbookings) => {
+    customer.bookings =  allbookings.filter(booking => booking.userID === customer.id);
     return customer
+}
+
+export const findOpenBookings = (date, bookings) => {
+    return bookings.map(booking => {
+        if(booking.date === date) {
+            return booking.roomNumber
+        }
+    })
 }
--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,36 @@
 import './css/styles.css';
 import './images/turing-logo.png'
 import { fetchData } from './api-calls';
+import { findBookings } from './bookings';
+import { matchRooms } from './rooms';
+import { totalSpend } from './customer';
 
 const bookingsButton = document.querySelector('#bookings-button');
 const billingsButton = document.querySelector('#billings-button');
+const bookRoomButton = document.querySelector('#book-room-button');
+const submitButton = document.querySelector('#submit-button');
 
+const bookingsCard = document.querySelectorAll('.booking-card');
+const billingCard = document.querySelectorAll('.billing-card');
 const bookingsContainer = document.querySelector('.bookings-container');
 const billingsContainer = document.querySelector('.billings-container');
+const bookARoomContainer = document.querySelector('.book-a-room-container');
+
 
 let allCustomers;
 let allBookings;
 let allRooms;
 let currentCustomer0;
 let currentCustomer;
+let bookDate;
+let bookedRoom;
 
 window.addEventListener('load', () => {
     retrieveData();
+});
+
+bookRoomButton.addEventListener('click', () => {
+renderBookRoom();
 });
 
 bookingsButton.addEventListener('click', () => {
@@ -24,6 +39,10 @@ bookingsButton.addEventListener('click', () => {
 
 billingsButton.addEventListener('click', () => {
     rendershowBillings();
+});
+
+submitButton.addEventListener('click', () => {
+submitBooking();
 });
 
 function retrieveData() {
@@ -45,21 +64,82 @@ function retrieveData() {
             allRooms = data[2].rooms;
             currentCustomer0 = allCustomers[23];
         })
+        .then(() => {
+            customerObject();
+        })
         .catch(err => console.log(err.message, err));
 }
 
+const sendBookedRoom = (url) => {
+    
+    const data = {
+      userID: parseInt(currentCustomer.id),
+      date: `${bookDate.value.replaceAll('-', '/')}`,
+      roomNumber: parseInt(bookedRoom[0].number),
+    };
+
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  };
+
 function customerObject () {
-    currentCustomer1st = findBookings(currentCustomer0);
-    currentCustomer2nd = matchRooms(currentCustomer1st);
+    let currentCustomer1st = findBookings(currentCustomer0, allBookings);
+    let currentCustomer2nd = matchRooms(currentCustomer1st, allRooms);
     currentCustomer = totalSpend(currentCustomer2nd);
+    console.log(currentCustomer);
 }
 
 function rendershowBillings() {
     billingsContainer.classList.remove('hidden');
     bookingsContainer.classList.add('hidden');
+
+    billingsContainer.innerHTML =' ';
+    
+    billingsContainer.innerHTML += currentCustomer.rooms.map(room => {
+        return `
+        <div class="billing-card">
+            <p>Room Number: ${room.number}</p>
+            <p>Room Type: ${room.roomType}</p>
+            <p>Cost per Night: $${room.costPerNight}</p>
+        </div>
+            `
+    }).join('');
 }
 
 function rendershowBookings() {
     bookingsContainer.classList.remove('hidden');
     billingsContainer.classList.add('hidden');
+
+    bookingsContainer.innerHTML =' ';
+    bookingsContainer.innerHTML += `
+    <div class="total-container">
+        <h2 class="totalSign">Total Cost: ${currentCustomer.total}</h2>
+      </div>
+    `
+    bookingsContainer.innerHTML += currentCustomer.bookings.map(booking => {
+        return `
+        <div class="booking-card">
+            <p>Room Number: ${booking.roomNumber}</p>
+            <p>Date: ${booking.date}</p>
+        </div>
+        `
+    }).join('');
+}
+
+function submitBooking() {
+    
+}
+
+function renderBookRoom() {
+    bookingsContainer.classList.add('hidden');
+    billingsContainer.classList.add('hidden');
+    bookARoomContainer.classList.remove('hidden');
+   
+    
+    
 }
