2f07eb84d94308fe0a73eef50726c27698faac5f
Florenzo Bauer <florenzobauer@gmail.com>
2024-01-25 16:17:37 -0700

    Updates

--- bundle.js
+++ bundle.js
@@ -502,7 +502,7 @@ const getPromises = [
     fetch('https://murmuring-plateau-60579-55a1e980f710.herokuapp.com/api/v1/bookings'),
     fetch('https://murmuring-plateau-60579-55a1e980f710.herokuapp.com/api/v1/rooms')
 ];
-
+// this is a comment for no reason that to redeploy
 const fetchData = () => {
     return Promise.all(getPromises);
 }
