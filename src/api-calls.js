f9e8780d58ab930b7609700d8af8c8a704e8a2a7
Florenzo Bauer <florenzobauer@gmail.com>
2024-01-25 16:16:56 -0700

    Trying to redeploy

--- src/api-calls.js
+++ src/api-calls.js
@@ -3,7 +3,7 @@ const getPromises = [
     fetch('https://murmuring-plateau-60579-55a1e980f710.herokuapp.com/api/v1/bookings'),
     fetch('https://murmuring-plateau-60579-55a1e980f710.herokuapp.com/api/v1/rooms')
 ];
-
+// this is a comment for no reason that to redeploy
 export const fetchData = () => {
     return Promise.all(getPromises);
 }
